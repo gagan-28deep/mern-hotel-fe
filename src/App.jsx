205c9791c6497a59fb8cf25571ccf74e0602ca90
gagan-28deep <gagan_28_deep@outlook.com>
2024-06-08 18:30:31 +0530

    error resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,50 @@
+// import Footer from "./components/Footer";
+// import Header from "./components/Header";
+// import Hero from "./components/Hero";
+// import Layout from "./layouts/Layout";
+// import { Outlet } from "react-router-dom";
+// import { useDispatch } from "react-redux";
+// import { useEffect } from "react";
+// import getStorage from "./services/storageService";
+// import {
+//   getUserSuccess,
+//   getIsAuthenticated,
+//   getAccessToken,
+//   getRefreshToken,
+// } from "./store/slices/userSlice";
+
+// import { getStripePromise } from "./store/slices/stripeSlices";
+
+// import { loadStripe } from "@stripe/stripe-js";
+// const stripe_pub_key = import.meta.env.VITE_STRIPE_PUB_KEY || "";
+// const stripePromise = await loadStripe(stripe_pub_key);
+
+// function App() {
+//   const dispatch = useDispatch();
+//   useEffect(() => {
+//     const userData = getStorage("user");
+//     const accessToken = getStorage("accessToken");
+//     const refreshToken = getStorage("refreshToken");
+//     dispatch(getStripePromise(stripePromise));
+//     if (userData && accessToken && refreshToken) {
+//       dispatch(getUserSuccess(userData));
+//       dispatch(getAccessToken(accessToken));
+//       dispatch(getRefreshToken(refreshToken));
+//       dispatch(getIsAuthenticated(true));
+//     }
+//   }, [dispatch]);
+//   return (
+//     <>
+//       <Layout>
+//         <Outlet />
+//       </Layout>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
@@ -17,22 +64,30 @@ import { getStripePromise } from "./store/slices/stripeSlices";
 
 import { loadStripe } from "@stripe/stripe-js";
 const stripe_pub_key = import.meta.env.VITE_STRIPE_PUB_KEY || "";
-const stripePromise = await loadStripe(stripe_pub_key);
 
 function App() {
   const dispatch = useDispatch();
+
   useEffect(() => {
+    async function initializeStripe() {
+      const stripePromise = await loadStripe(stripe_pub_key);
+      dispatch(getStripePromise(stripePromise));
+    }
+
     const userData = getStorage("user");
     const accessToken = getStorage("accessToken");
     const refreshToken = getStorage("refreshToken");
-    dispatch(getStripePromise(stripePromise));
+
     if (userData && accessToken && refreshToken) {
       dispatch(getUserSuccess(userData));
       dispatch(getAccessToken(accessToken));
       dispatch(getRefreshToken(refreshToken));
       dispatch(getIsAuthenticated(true));
     }
+
+    initializeStripe();
   }, [dispatch]);
+
   return (
     <>
       <Layout>
